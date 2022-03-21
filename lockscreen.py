d1ed8e6ae4b608f7172702538416aee86678cafb
xansgithub <farleyalex22@gmail.com>
2022-03-21 14:01:29 -0400

    fixed funny issue

--- lockscreen.py
+++ lockscreen.py
@@ -1,7 +1,7 @@
 # Alex Farley
 # arf2846@rit.edu
 # This program locks the workstation at random time intervals
-# between 30 and 60 seconds.
+# between 30 and 180 seconds.
 
 import ctypes
 import time
@@ -12,15 +12,15 @@ import os
 # ctypes.windll.user32.LockWorkStation()
 
 # define variables
-user32 = ctypes.windll.user32   # for Windows
 bindows = sys.platform          # for getting OS
 rando = 0
 
 # be annoying
 while True:
-    rando = random.randint(30, 60)
+    rando = random.randint(30, 180)
     time.sleep(rando)
     if bindows == "win32":
+        user32 = ctypes.windll.user32   # for Windows
         user32.LockWorkStation()
     elif bindows == "linux":
         os.system("xdg-screensaver lock")
